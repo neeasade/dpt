f8b10ca27b61ec9423b511a2aca02c93535b8bbd
neeasade <nathanisom27@gmail.com>
2017-07-03 20:29:09 -0500

    cli parsing, celluar automata arguments

--- 050/src/gol/core.clj
+++ 050/src/gol/core.clj
@@ -1,16 +1,11 @@
 (ns gol.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-; this is an example - current program reads from file in this format.
-(def startboard
-  "
-  .....
-  .....
-  .XXX.
-  .....
-  .....
-  "
-  )
+; globals (defaults handled by args)
+(def born nil)
+(def survive nil)
+(def interval nil)
 
 (defn convert-board
   "take board as string and convert to nested vector bools"
@@ -67,14 +62,10 @@
 
 (defn get-next-state
   "get the next state"
-  [x y current board]
-  (case (count-live-neighbors x y board)
-    0 false
-    1 false
-    2 (and current true)
-    3 true
-    false
-    )
+  [x y alive board]
+  (some #(= (count-live-neighbors x y board) %)
+        (if alive survive born)
+        )
   )
 
 (defn render-board [board]
@@ -89,7 +80,6 @@
        )
   )
   )
-
 
 (defn get-next-board
 [board]
@@ -110,20 +100,59 @@
   (print (str (char 27) "[2J")) ; clear screen
   (print (str (char 27) "[;H")) ; move cursor to the top left corner of the screen
   (render-board board)
-  (Thread/sleep 500)
+  (Thread/sleep interval)
   (get-next-board board)
   )
 
 (defn run [board]
-  (recur (step board)
+  (recur (step board))
   )
+
+(def cli-options
+  [
+   ["-f" "--file FILE" "File containing startmap"
+   :default "default.map"]
+
+   ["-i" "--interval INTERVAL" "Sleep interval between generations (in milliseconds)"
+    :default 500
+    :parse-fn #(Integer/parseInt %)]
+
+   ["-s" "--survive SURVIVE_COUNT" "valid numbers for survival"
+    :default "23"]
+
+   ["-b" "--born BORN_COUNT" "valid numbers for cell to be born"
+    :default "3"]
+
+   ["-h" "--help"]
+   ]
+  )
+
+; couldn't get this to work as anon function in tools.cli - worked in repl as such..
+(defn numberstring-to-vector
+  "turn strings of numbers into vectors of numbers."
+  [input]
+  (into [] (map #(Integer/parseInt (str %)) input))
   )
 
 ;; some good example inputs can be found at the bottom of this page:
 ;; http://www.cs.nyu.edu/courses/fall13/CSCI-GA.1133-001/HW/game-of-life.html
 (defn -main
-  [target & args]
-  (run
-   (convert-board (slurp target))
-   )
+  [& args]
+  (let [arg-result (parse-opts args cli-options)
+        options (:options arg-result)
+        help-text (:summary arg-result)
+        ]
+
+    (if (:help options)
+      (println help-text)
+
+      (do
+        (def interval (:interval options))
+        (def survive (numberstring-to-vector (:survive options)))
+        (def born (numberstring-to-vector (:born options)))
+
+        (run (convert-board (slurp (:file options))))
+        )
+      )
+    )
   )
